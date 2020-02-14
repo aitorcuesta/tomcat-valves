c5d3084ca895e0b5787b212d5a2a9a475ec240ed
aitorcuesta <aitor.cuesta@gmail.com>
2020-02-14 14:28:31 +0100

    tomcat-6.0.53

--- src/main/java/es/aitorcuesta/tomcat/sso/NoMasLogin.java
+++ src/main/java/es/aitorcuesta/tomcat/sso/NoMasLogin.java
@@ -16,19 +16,19 @@ import org.apache.catalina.valves.ValveBase;
 public class NoMasLogin extends ValveBase {
 
 	private static final String FAKE_USER_STRING = "ssoUser";
-	private static final Map ssoMap = new HashMap();
 
+	private static final Map<String, Principal> ssoMap = new HashMap<String, Principal>();
+
+	@Override
 	public void invoke(Request request, Response response) throws IOException, ServletException {
 		String fakeUser = request.getParameter(FAKE_USER_STRING);
 		String contextApp = request.getRequestURI().split("/")[1];
 		if (null != fakeUser && fakeUser.length() > 0) {
-			// final Principal principal = new GenericPrincipal(fakeUser, "", new
-			// ArrayList<String>());
-			final Principal principal = new GenericPrincipal(null, fakeUser, "", new ArrayList());
+			final Principal principal = new GenericPrincipal(null, fakeUser, "", new ArrayList<String>());
 			ssoMap.put(contextApp, principal);
 		}
-		request.setUserPrincipal((Principal) ssoMap.get(contextApp));
+		request.setUserPrincipal(ssoMap.get(contextApp));
 		getNext().invoke(request, response);
 	}
-
+	
 }
